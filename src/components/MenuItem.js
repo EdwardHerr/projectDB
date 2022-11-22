b6cf04f024e935485a84982c1deccf77d946abad
Gunn Hwang <gunhwang99@gmail.com>
2022-11-22 03:22:27 -0800

    add product page

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,7 +2,8 @@ import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function MenuItem() {
-  const [menuItem, setMenuItem] = useState();
+  const [menuItem, setMenuItem] = useState({});
+  const [qty, setQty] = useState(1);
 
   const fetchData = async () => {
     axios.get(window.location.pathname).then((res) => {
@@ -13,11 +14,37 @@ export default function MenuItem() {
 
   useEffect(() => {
     fetchData();
-  }, [menuItem]);
+  }, []);
 
   return (
-    <div className='col-sm-4 mb-5'>
-      <h1>hello {window.location.pathname}</h1>
+    <div className='container col-sm-4 mb-5'>
+      <h1>{menuItem.name}</h1>
+      <p>{menuItem.description}</p>
+      <p>{menuItem.listPrice}</p>
+      <div className='row'>
+        <div className='qty col input-group'>
+          <button
+            className='btn btn-outline-secondary'
+            onClick={() =>
+              setQty((curr) => {
+                if (curr > 1) {
+                  return curr - 1;
+                }
+                return curr;
+              })
+            }
+          >
+            -
+          </button>
+          <input type='text' className='form-control' value={qty} disabled readonly />
+          <button className='btn btn-outline-secondary' onClick={() => setQty(qty + 1)}>
+            +
+          </button>
+        </div>
+        <a href='#' className='col-9 btn btn-primary'>
+          Add to Cart
+        </a>
+      </div>
     </div>
   );
 }
