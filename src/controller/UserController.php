f5eaab91cf54da42581a4554b9fdb820a797f6e2
Timothy Patawaran <tpatawaran@outlook.com>
2022-11-24 03:01:30 +0000

    update api

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -10,6 +10,9 @@ class UserController {
 
     public function processRequest() {
         switch ($this->requestMethod) {
+            case 'POST':
+                $response = $this->updateUserInfo();
+                break;
             case 'GET':
                 if ($this->userId) {
                     $response = $this->getUserInfo($this->userId);
@@ -23,6 +26,62 @@ class UserController {
         if ($response['body']) {
             echo $response['body'];
         }
+    }
+    
+    private function updateUserInfo($userId) {
+        $input = json_decode(file_get_contents('php://input'));
+        if (! $this->validateUser($input)) {
+            return $this->invalidUserResponse();
+        }
+        $res = $this->UserDB::updateUser($input);
+            if ($res) {
+                $response['status_code_header'] = 'HTTP/1.1 201 Created';
+                $body = "Update successful!";
+            } else {
+                $response['status_code_header'] = 'HTTP/1.1 400 Bad Request';
+                $body = json_encode([
+                    'error' => 'Unable to update account'
+                ]);
+            }
+            $response['body'] = $body;
+            return $response;
+    }
+    
+    
+    private function validateUser($input) {
+        if (! isset($input->username) || strlen($input->username) < 1) {
+            return false;
+        }
+        if (! isset($input->firstName) || strlen($input->firstName) < 1) {
+            return false;
+        }
+        if (! isset($input->lastName) || strlen($input->lastName) < 1) {
+            return false;
+        }
+        if (! isset($input->inputEmail) || strlen($input->inputEmail) < 1) {
+            return false;
+        }
+        if (! isset($input->inputAddress) || strlen($input->inputAddress) < 1) {
+            return false;
+        }
+        if (! isset($input->inputPassword) || strlen($input->inputPassword) < 1) {
+            return false;
+        }
+        if (! isset($input->confirmPassword) || strlen($input->confirmPassword) < 1) {
+            return false;
+        }
+        if ($input->confirmPassword !== $input->inputPassword) {
+            return false;
+        }
+        return true;
+    }
+    
+    private function invalidUserResponse() {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid User'
+        ]);
+        return $response;
     }
 
     private function getUserInfo($userId) {
